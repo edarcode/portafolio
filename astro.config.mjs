52105678fcba23207c074bdfa5f9b0b9ebb5696e
Edwin Ortiz <edarcode@gmail.com>
2024-10-29 16:36:41 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,14 +11,6 @@ export default defineConfig({
       "/": "./src/pages/index/index.astro",
       "/utils": "./src/pages/utils/utils.astro",
       "/look": "./src/pages/look/look.astro",
-
-      // Reuse the same entrypoint for more than one route for more precise control
-      // here defining that the route should be only on the tag index and with a single segment
-      tag: "./src/routes/tags.astro",
-      "tag/[tag]": "./src/routes/tags.astro",
-
-      // Publish common Astro files shared across projects as dependencies and use them here
-      compliance: "@company/legal-pages/compliance.astro",
     }),
   ],
 });
