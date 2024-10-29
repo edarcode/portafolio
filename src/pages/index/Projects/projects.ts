d60cf35a8710b0db47275eb4d18f81525bae1ac0
Edwin Ortiz <edarcode@gmail.com>
2024-10-29 17:42:41 -0500

    agg proyectos

---
+++ src/pages/index/Projects/projects.ts
@@ -0,0 +1,44 @@
+export const PROJECTS = [
+  {
+    title: "Chat",
+    dev: "Full-Stack",
+    deploy: "",
+    description:
+      "Chat online. Envía mensajes, ya sea con tus amigos, familia o compañeros de trabajo, todo desde una sola aplicación. Los datos persisten en una base de datos.",
+    tools:
+      "Vite, React, TS, TanStack Query, React Hook Form, Node.js, Express, DrizzleORM, SQLite, WebSockets.",
+    imgs: {
+      mobile:
+        "https://res.cloudinary.com/dvmsllkkg/image/upload/v1729197397/edarcode/tk3oadwm7k3ctyc9tey8.png",
+      pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1729197397/edarcode/ui4gal77ioq3cmrvf7bw.png",
+    },
+  },
+
+  {
+    title: "Contacts",
+    dev: "Full-Stack",
+    deploy: "",
+    description:
+      "Diseñada para ayudar a las personas a gestionar sus contactos. Permite listar, agregar, editar y eliminar contactos. Los datos persisten en una base de datos alojada en Turso.",
+    tools:
+      "Vite, React, TS, TanStack Query, React Hook Form, Node.js, Express, DrizzleORM, SQLite.",
+    imgs: {
+      mobile:
+        "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/contacts_mobile.png",
+      pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/contacts_desktop.png",
+    },
+  },
+  {
+    title: "Notaku",
+    dev: "Front-End",
+    deploy: "https://notaku.vercel.app/",
+    description:
+      "Encontrarás las últimas noticias, novedades del mundo del anime, junto con una herramienta de búsqueda que permite filtrar animes por título, género, popularidad y mucho más. Los datos son consultados en la API de kitsu.",
+    tools: "Vite, React, TS, TanStack Query, React Hook Form.",
+    imgs: {
+      mobile:
+        "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_mobile.png",
+      pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_desktop.png",
+    },
+  },
+];
