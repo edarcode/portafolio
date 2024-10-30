6309bac0251bde2f651d9c763c166c15fdde589b
Edwin Ortiz <edarcode@gmail.com>
2024-10-29 19:54:36 -0500

    agg utilidades

---
+++ src/pages/utils/useful.ts
@@ -0,0 +1,38 @@
+export const USEFUL_LINKS = [
+  { href: "https://imgto.xyz/", display: "Optimizar archivos" },
+  { href: "https://www.svgrepo.com/", display: "SVG grepo" },
+  { href: "https://svgl.app/", display: "SVG de @pheralb" },
+  { href: "https://console.cloudinary.com/", display: "Cloudinary" },
+  { href: "https://www.prisma.io/docs/orm", display: "Prisma ORM" },
+  { href: "https://manz.dev/", display: "manzdev" },
+  { href: "https://midu.dev/", display: "midudev" },
+  {
+    href: "https://cursos.doriandesings.com/course/css-intermedio",
+    display: "CSS intermedio dorian",
+  },
+  { href: "https://pagespeed.web.dev", display: "Lighthouse" },
+  { href: "https://www.freepik.es/", display: "Freepik (IMGS)" },
+  { href: "https://www.pexels.com/es-es/", display: "Pexel (IMGS)" },
+  { href: "https://www.drawdb.app/", display: "Diagramas db" },
+  { href: "https://drizzle.run/visualizer", display: "Diagramas db drizzle" },
+  {
+    href: "https://number-conversion-tau.vercel.app/",
+    display: "Conversor de números (edarcode)",
+  },
+  {
+    href: "https://calculator-ten-khaki-94.vercel.app/",
+    display: "Calculadora (edarcode)",
+  },
+  { href: "https://ideogram.ai/", display: "Gerador de img (IA)" },
+  {
+    href: "https://notaku.vercel.app/",
+    display: "Sitio web de animes (edarcode)",
+  },
+  {
+    href: "https://www.befunky.com/es/crear/editor-de-fotos/",
+    display: "Editor de fotos",
+  },
+  { href: "https://neumorphism.io", display: "Neumorphism" },
+  { href: "https://icon-sets.iconify.design/", display: "Iconify" },
+  { href: "https://jakearchibald.github.io/svgomg/", display: "Optimizar svg" },
+];
