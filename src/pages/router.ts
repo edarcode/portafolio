957bb8e22039f71339252d70c267fb3414b4c110
Edwin Ortiz <edarcode@gmail.com>
2024-10-31 18:44:28 -0500

    refactor routes

--- src/pages/router.ts
+++ src/pages/router.ts
@@ -1,16 +1,14 @@
-import { CLIENT_BASE_URL } from "../consts/urls";
-
 export const ROUTER = {
   index: {
-    href: `${CLIENT_BASE_URL}/`,
+    href: "/",
     display: "Inicio",
   },
   look: {
-    href: `${CLIENT_BASE_URL}/look`,
+    href: "/look",
     display: "Componentes",
   },
   utils: {
-    href: `${CLIENT_BASE_URL}/utils`,
+    href: "/utils",
     display: "Utilidades",
   },
 };
